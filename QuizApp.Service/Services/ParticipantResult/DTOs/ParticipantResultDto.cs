176ac44e268498d475b205fdd330b82f52d5c0b3
Phat Dang <dangthanhphat2303197@gmail.com>
2023-06-16 08:12:01 +0700

    update ParticipantResultDto

--- QuizApp.Service/Services/ParticipantResult/DTOs/ParticipantResultDto.cs
+++ QuizApp.Service/Services/ParticipantResult/DTOs/ParticipantResultDto.cs
@@ -6,9 +6,5 @@
         public int? Score { get; set; }
         public int? TimeTaken { get; set; }
         public bool IsFinish { get; set; }
-        public Guid ParticipantId { get; set; }
-        public Guid QuestionGroupId { get; set; }
-        public virtual QuestionGroupDto QuestionGroup { get; set; }
-        public virtual ParticipantDto Participant { get; set; }
     }
 }
